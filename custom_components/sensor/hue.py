253e2a9d9663e26a7a53e2affa40dec7e40d5772
Søren Christian Aarup <scaa@mbp.local>
2019-01-06 10:11:03 +0100

    modified:   custom_components/sensor/hue.py

--- custom_components/sensor/hue.py
+++ custom_components/sensor/hue.py
@@ -4,133 +4,112 @@ Sensor for checking the status of Hue sensors.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.hue/
 """
-import asyncio
-import async_timeout
 import logging
-import threading
 from datetime import timedelta
 
+import requests
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (CONF_IP_ADDRESS, CONF_TOKEN)
 from homeassistant.helpers.entity import Entity
-from homeassistant.exceptions import PlatformNotReady
-from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.util import Throttle
 
-DEPENDENCIES = ["hue"]
-
-__version__ = "1.0.2"
+__version__ = '0.8'
 
 _LOGGER = logging.getLogger(__name__)
 
-SCAN_INTERVAL = timedelta(seconds=0.1)
-TYPE_GEOFENCE = "Geofence"
-ICONS = {"SML": "mdi:run-fast", "RWL": "mdi:remote", "ZGP": "mdi:remote"}
-ATTRS = {
-    "SML": [
-        "light_level",
-        "battery",
-        "last_updated",
-        "lx",
-        "dark",
-        "daylight",
-        "temperature",
-        "on",
-        "reachable",
-    ],
-    "RWL": ["last_updated", "battery", "on", "reachable"],
-    "ZGP": ["last_updated"],
-}
+SCAN_INTERVAL = timedelta(seconds=60.0)
 
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_IP_ADDRESS): cv.string,
+    vol.Required(CONF_TOKEN): cv.string,
+})
+names = {}
 
-def parse_hue_api_response(sensors):
+def parse_hue_api_response(response):
     """Take in the Hue API json response."""
-    data_dict = {}  # The list of sensors, referenced by their hue_id.
-
+    data_dict = {}    # The list of sensors, referenced by their hue_id.
+    # Hue only writes the name of the presence sensor and not temperature and light.
+    for key in response.keys():
+        sensor = response[key]
+        if sensor['type'] == 'ZLLPresence':
+            global names
+            uid = sensor['uniqueid'][0:25]
+            name = sensor['name']
+            names[uid] = name
     # Loop over all keys (1,2 etc) to identify sensors and get data.
-    for sensor in sensors:
-        modelid = sensor["modelid"][0:3]
-        if modelid in ["RWL", "SML", "ZGP"]:
-            _key = modelid + "_" + sensor["uniqueid"][:-5]
-            if modelid == "RWL":
-                data_dict[_key] = parse_rwl(sensor)
-            elif modelid == "ZGP":
-                data_dict[_key] = parse_zgp(sensor)
-            elif modelid == "SML":
-                if _key not in data_dict:
+    for key in response.keys():
+        sensor = response[key]
+        modelid = sensor['modelid'][0:3]
+        if modelid in ['RWL', 'SML', 'ZGP']:
+            _key = modelid + '_' + sensor['uniqueid'][:-5]
+
+            if modelid == 'RWL':
+                continue
+                #data_dict[_key] = parse_rwl(sensor)
+            elif modelid == 'ZGP':
+                continue
+                #data_dict[_key] = parse_zgp(sensor)
+            elif modelid == 'SML':
+                if _key not in data_dict.keys():
                     data_dict[_key] = parse_sml(sensor)
                 else:
                     data_dict[_key].update(parse_sml(sensor))
 
+        elif sensor['modelid'] == 'HA_GEOFENCE':
+            data_dict['Geofence'] = parse_geofence(sensor)
     return data_dict
-
 
 def parse_sml(response):
     """Parse the json for a SML Hue motion sensor and return the data."""
-    if response["type"] == "ZLLLightLevel":
-        lightlevel = response["state"]["lightlevel"]
+    data = {}
+    if response['type'] == "ZLLLightLevel":
+        lightlevel = response['state']['lightlevel']
         if lightlevel is not None:
-            lx = round(float(10 ** ((lightlevel - 1) / 10000)), 2)
-            dark = response["state"]["dark"]
-            daylight = response["state"]["daylight"]
-            data = {
-                "light_level": lightlevel,
-                "lx": lx,
-                "dark": dark,
-                "daylight": daylight,
-            }
+            lx = round(float(10**((lightlevel-1)/10000)), 2)
+            dark = response['state']['dark']
+            daylight = response['state']['daylight']
+            data = {'light_level': lightlevel,
+                    'lx': lx,
+                    'dark': dark,
+                    'daylight': daylight, }
         else:
-            data = {
-                "light_level": "No light level data",
-                "lx": None,
-                "dark": None,
-                "daylight": None,
-            }
+            data = {'light_level': 'No light level data'}
 
-    elif response["type"] == "ZLLTemperature":
-        if response["state"]["temperature"] is not None:
-            data = {"temperature": response["state"]["temperature"] / 100.0}
+    elif response['type'] == "ZLLTemperature":
+        uid = response['uniqueid'][0:25]
+        name = names[uid]
+        if response['state']['temperature'] is None:
+            state = 'null'
         else:
-            data = {"temperature": "No temperature data"}
+            state = response['state']['temperature']/100.0
 
-    elif response["type"] == "ZLLPresence":
-        name_raw = response["name"]
-        arr = name_raw.split()
-        arr.insert(-1, "motion")
-        name = " ".join(arr)
-        hue_state = response["state"]["presence"]
-        if hue_state is True:
-            state = "on"
-        else:
-            state = "off"
-
-        data = {
-            "model": "SML",
-            "name": name,
-            "state": state,
-            "battery": response["config"]["battery"],
-            "on": response["config"]["on"],
-            "reachable": response["config"]["reachable"],
-            "last_updated": response["state"]["lastupdated"].split("T"),
-        }
+        data = {'model': 'SML',
+                'unit_of_measurement': '°C',
+                'name': 'temperature_'+name,
+                'state': state,
+                'battery': response['config']['battery'],
+                'on': response['config']['on'],
+                'reachable': response['config']['reachable'],
+                'last_updated': response['state']['lastupdated'].split('T')}
     return data
-
 
 def parse_zgp(response):
     """Parse the json response for a ZGPSWITCH Hue Tap."""
-    TAP_BUTTONS = {34: "1_click", 16: "2_click", 17: "3_click", 18: "4_click"}
-    press = response["state"]["buttonevent"]
+    TAP_BUTTONS = {34: '1_click', 16: '2_click', 17: '3_click', 18: '4_click'}
+    press = response['state']['buttonevent']
     if press is None:
-        button = "No data"
+        button = 'No data'
     else:
         button = TAP_BUTTONS[press]
 
-    data = {
-        "model": "ZGP",
-        "name": response["name"],
-        "state": button,
-        "last_updated": response["state"]["lastupdated"].split("T"),
-    }
+    data = {'model': 'ZGP',
+            'name': 'huetap_'+response['name'],
+            'state': button,
+            'last_updated': response['state']['lastupdated'].split('T')}
     return data
-
 
 def parse_rwl(response):
     """Parse the json response for a RWL Hue remote."""
@@ -139,156 +118,158 @@ def parse_rwl(response):
         I know it should be _released not _up
         but _hold_up is too good to miss isn't it
     """
-    responsecodes = {"0": "_click", "1": "_hold", "2": "_click_up", "3": "_hold_up"}
+    responsecodes = {
+        '0' : "_click",
+        '1' : "_hold",
+        '2' : "_click_up",
+        '3' : "_hold_up"
+    }
 
     button = ""
-    if response["state"]["buttonevent"]:
-        press = str(response["state"]["buttonevent"])
+    if response['state']['buttonevent']:
+        press = str(response['state']['buttonevent'])
         button = str(press)[0] + responsecodes[press[-1]]
 
-    data = {
-        "model": "RWL",
-        "name": response["name"],
-        "state": button,
-        "battery": response["config"]["battery"],
-        "on": response["config"]["on"],
-        "reachable": response["config"]["reachable"],
-        "last_updated": response["state"]["lastupdated"].split("T"),
-    }
+    data = {'model': 'RWL',
+            'name': 'hueswitch_'+response['name'],
+            'state': button,
+            'battery': response['config']['battery'],
+            'on': response['config']['on'],
+            'reachable': response['config']['reachable'],
+            'last_updated': response['state']['lastupdated'].split('T')}
+    return data
+
+def parse_geofence(response):
+    """Parse the json response for a GEOFENCE and return the data."""
+    hue_state = response['state']['presence']
+    if hue_state is True:
+        state = 'on'
+    else:
+        state = 'off'
+    data = {'name': response['name'],
+            'model': 'Geofence',
+            'state': state}
     return data
 
 
-def get_bridges(hass):
-    from homeassistant.components import hue
-    from homeassistant.components.hue.bridge import HueBridge
-
-    return [
-        entry
-        for entry in hass.data[hue.DOMAIN].values()
-        if isinstance(entry, HueBridge) and entry.api
-    ]
-
-
-async def update_api(api):
-    import aiohue
-
-    try:
-        with async_timeout.timeout(10):
-            await api.update()
-    except (asyncio.TimeoutError, aiohue.AiohueException) as err:
-        _LOGGER.debug("Failed to fetch sensors: %s", err)
-        return False
-    return True
-
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Hue sensors."""
-    data = HueSensorData(hass, async_add_entities)
-    result = await data.async_update_info()
-    if not result:
-        raise PlatformNotReady
-    async_track_time_interval(hass, data.async_update_info, SCAN_INTERVAL)
+    try:
+        data = HueSensorData(parse_hue_api_response, config)
+        data.update()
+        sensors = []
+        for key in data.data.keys():
+            _LOGGER.warning(HueSensor(key, data))
+            sensors.append(HueSensor(key, data))
+        add_devices(sensors, True)
+    except Exception as e:
+        print(e)
 
 
 class HueSensorData(object):
     """Get the latest sensor data."""
 
-    def __init__(self, hass, async_add_entities):
+    def __init__(self, parse_hue_api_response, config):
         """Initialize the data object."""
-        self.hass = hass
-        self.lock = threading.Lock()
-        self.data = {}
-        self.sensors = {}
-        self.async_add_entities = async_add_entities
+        self.url = self._build_url(config)
+        self.data = None
+        self.parse_hue_api_response = parse_hue_api_response
 
-    async def update_bridge(self, bridge):
-        available = await update_api(bridge.api.sensors)
-        if not available:
-            return
+    # Update only once in scan interval.
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        """Get the latest data."""
+        response = requests.get(self.url)
+        if response.status_code != 200:
+            _LOGGER.warning("Invalid response from API")
+        else:
+            self.data = self.parse_hue_api_response(response.json())
 
-        data = parse_hue_api_response(
-            sensor.raw
-            for sensor in bridge.api.sensors.values()
-            if sensor.type != TYPE_GEOFENCE
-        )
+    def _build_url(self, config):
+        ip_address = config.get(CONF_IP_ADDRESS)
+        token = config.get(CONF_TOKEN)
 
-        new_sensors = data.keys() - self.data.keys()
-        updated_sensors = [
-            key
-            for key, new in data.items()
-            if key in self.data and self.data[key] != new
-        ]
-        self.data.update(data)
-
-        new_entities = {
-            entity_id: HueSensor(entity_id, self) for entity_id in new_sensors
-        }
-        if new_entities:
-            _LOGGER.debug("Created %s", ", ".join(new_entities.keys()))
-            self.sensors.update(new_entities)
-            self.async_add_entities(new_entities.values(), True)
-        for entity_id in updated_sensors:
-            self.sensors[entity_id].async_schedule_update_ha_state()
-
-    async def async_update_info(self, now=None):
-        """Get the bridge info."""
-        locked = self.lock.acquire(False)
-        if not locked:
-            return
-        try:
-            bridges = get_bridges(self.hass)
-            if not bridges:
-                return
-            await asyncio.wait(
-                [self.update_bridge(bridge) for bridge in bridges], loop=self.hass.loop
-            )
-        finally:
-            self.lock.release()
-        return True
+        return "http://" + ip_address + "/api/" + token + "/sensors"
 
 
 class HueSensor(Entity):
     """Class to hold Hue Sensor basic info."""
 
-    ICON = "mdi:run-fast"
+    ICON = 'mdi:thermometer'
 
     def __init__(self, hue_id, data):
         """Initialize the sensor object."""
         self._hue_id = hue_id
-        self._data = data.data  # data is in .data
-
-    @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
+        self._data = data    # data is in .data
+        self._icon = None
+        self._unit_of_measurement = '°C'
+        self._name = self._data.data[self._hue_id]['name']
+        self._model = self._data.data[self._hue_id]['model']
+        self._state = self._data.data[self._hue_id]['state']
+        self._attributes = {}
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        data = self._data.get(self._hue_id)
-        if data:
-            return data["name"]
+        return self._name
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        data = self._data.get(self._hue_id)
-        if data:
-            return data["state"]
+        return self._state
 
     @property
     def icon(self):
         """Icon to use in the frontend, if any."""
-        data = self._data.get(self._hue_id)
-        if data:
-            icon = ICONS.get(data["model"])
-            if icon:
-                return icon
-        return self.ICON
+        return self._icon
+
+    @property
+    def unit_of_measurement(self):
+        return self._unit_of_measurement
 
     @property
     def device_state_attributes(self):
         """Attributes."""
-        data = self._data.get(self._hue_id)
-        if data:
-            return {key: data.get(key) for key in ATTRS.get(data["model"], [])}
+        return self._attributes
+
+    def update(self):
+        """Update the sensor."""
+        self._data.update()
+        self._state = self._data.data[self._hue_id]['state']
+        try:
+            if self._model == 'SML':
+                self._icon = 'mdi:thermometer'
+                self._attributes['light_level'] = self._data.data[
+                    self._hue_id]['light_level']
+                self._attributes['battery'] = self._data.data[
+                    self._hue_id]['battery']
+                self._attributes['last_updated'] = self._data.data[
+                    self._hue_id]['last_updated']
+                self._attributes['lx'] = self._data.data[
+                    self._hue_id]['lx']
+                self._attributes['dark'] = self._data.data[
+                    self._hue_id]['dark']
+                self._attributes['daylight'] = self._data.data[
+                    self._hue_id]['daylight']
+                self._attributes['on'] = self._data.data[
+                    self._hue_id]['on']
+                self._attributes['reachable'] = self._data.data[
+                    self._hue_id]['reachable']
+            elif self._model == 'RWL':
+                self._icon = 'mdi:remote'
+                self._attributes['last_updated'] = self._data.data[
+                    self._hue_id]['last_updated']
+                self._attributes['battery'] = self._data.data[
+                    self._hue_id]['battery']
+                self._attributes['on'] = self._data.data[
+                    self._hue_id]['on']
+                self._attributes['reachable'] = self._data.data[
+                    self._hue_id]['reachable']
+            elif self._model == 'ZGP':
+                self._icon = 'mdi:remote'
+                self._attributes['last_updated'] = self._data.data[
+                    self._hue_id]['last_updated']
+            elif self._model == 'Geofence':
+                self._icon = 'mdi:cellphone'
+        except Exception as e:
+            print(e)
